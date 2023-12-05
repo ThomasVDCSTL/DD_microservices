a87dd26f9aa4f27513672a1ad77a6fecb7cb5fce
Adil GAHRAMANLI <adil.gahramanli@Digital-Grenoble.local>
2023-12-05 11:34:21 +0100

    tout fonctionne

--- Gestion_joueurs/src/main/java/com/DD_microservices/Gestion_joueurs/Model/JoueurData.java
+++ Gestion_joueurs/src/main/java/com/DD_microservices/Gestion_joueurs/Model/JoueurData.java
@@ -1,8 +1,18 @@
 package com.DD_microservices.Gestion_joueurs.Model;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 
+@Schema(description = "Données du joueur",
+example = """
+        {
+            "id": 0,
+            "gamerName": "string",
+            "scoreGamer": 0
+          }
+        """)
 @Entity
 @Table(name="joueurdata")
 public class JoueurData {
@@ -13,6 +23,7 @@ public class JoueurData {
     private int id;
 
     @Column(name="nom")
+    @Size(min=3, max=25)
     private String gamerName;
 
     @Column
